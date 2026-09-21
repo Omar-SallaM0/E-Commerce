fd48e4f7a59f68382c3cc9c118b37865a4eaba57
Omar-SallaM0 <omarsallam906@gmail.com>
2026-09-21 22:28:46 +0300

    Registeration Done

--- src/app/components/auth-nav/auth-nav.component.ts
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-auth-nav',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
   templateUrl: './auth-nav.component.html',
   styleUrl: './auth-nav.component.scss'
 })
 export class AuthNavComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'login',
+        icon: 'pi pi-sign-in',
+        path: 'login',
+      },
+      {
+        label: 'register',
+        icon: 'pi pi-user-plus',
+        path: 'register',
+      },
+    ];
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [InputGroupModule, InputGroupAddonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  name!: FormControl
+  email!: FormControl
+  password!: FormControl
+  confirmPassword!: FormControl
+  registerForm!: FormGroup
 
+  initFormControls() {
+    this.name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+    this.confirmPassword = new FormControl('', [Validators.required, Validators.minLength(6), this.passwordMatchValidator.bind(this.password)]);
+  }
+
+  ngOnInit() {
+    this.initFormControls();
+    this.registerForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    });
+  }
+
+  passwordMatchValidator(pass: AbstractControl) : ValidatorFn {
+    return (control: AbstractControl): null | {[key: string]: boolean} => {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
+  }
+}
+  onSubmit(){
+    console.log(this.registerForm.value);
+  }
 }
